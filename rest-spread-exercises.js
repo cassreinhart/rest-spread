455e92403b650edb6d6f17d4737859f08e4f8773
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2022-08-20 12:37:18 -0500

    adding debugged functions

--- rest-spread-exercises.js
+++ rest-spread-exercises.js
@@ -1,29 +1,32 @@
-const filterOutOdds = () => {
+const filterOutOdds = (Array) => {
     const nums = [...Array];
-    return nums.filter((num) => num % 2 === 0);
+    return nums.filter((num) => num % 2 !== 0);
 };
 
 function findMin(...arguments) {
     return Math.min(...arguments)
 };
 
-function mergeObjects (obj1, obj2) {
-    return {...obj1, ...obj2};
+const mergeObjects = (obj1, obj2) => {
+    let newObj = {...obj1, ...obj2};
+    return newObj;
 };
 
-function doubleAndReturnArgs (array, ...arguments) {
-    return [array + ((...arguments) * 2)];
+const doubleAndReturnArgs = (array, ...arguments) => {
+    return [...array, ...arguments.map(val => val * 2)] ;
 };
 
-const removeRandoms = (...items) => {
-    const random = Math.random(...items);
-    return [...items, - random];
+const removeRandoms = items => {
+let random = Math.floor(Math.random()* items.length);
+    return [...items.slice(0, random), ...items.slice(random + 1)];
 };
 
 const extend = (array1, array2) => [...array1, ...array2];
 
 const addKeyVal = (obj, key, val) => {
-    return {...obj, key: `${val}`}; 
+    let newObj = {...obj};
+    newObj[key] = val;
+    return newObj 
 };
 
 const removeKey = (obj, key) => {
@@ -37,5 +40,7 @@ const combine = (obj1, obj2) => {
 };
 
 const update = (obj, key, val) => {
-    return {...obj, key:`${val}`}
+    const newObj = {...obj};
+    newObj[key] = val;
+    return newObj;
 }
